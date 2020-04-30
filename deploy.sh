207e0fedde519272653bd0e206e1786940be0533
MDAravindth <mdinesharavindth@gmail.com>
2020-04-30 22:16:28 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 CATALINA_HOME=/home/da/tomcat7
-$CATALINA_HOME/bin/shutdown.sh
-cp target/*.war $CATALINA_HOME/webapps/
-$CATALINA_HOME/bin/startup.sh
+sh ./$CATALINA_HOME/bin/shutdown.sh
+cp -r target/com.ms.calc.war $CATALINA_HOME/webapps/com.ms.calc.war
+sh ./$CATALINA_HOME/bin/startup.sh
